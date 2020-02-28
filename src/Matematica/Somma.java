0ac9d3aac5f0ecabe33bf3107aa6f8e12b665094
CarloP91 <carlopennetta91@gmail.com>
2020-02-28 16:40:01 +0100

    Start

---
+++ src/Matematica/Somma.java
@@ -0,0 +1,16 @@
+package Matematica;
+
+public class Somma {
+	
+	static int x = 1;
+	static int y = 2;
+	static int ris = x + y;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(ris);
+
+	}
+
+}
